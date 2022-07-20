e9797a2f4ad71422a36efb603bbb1df346af948f
slideit <slideit@users.noreply.github.com>
2022-07-20 01:36:26 +0000

    deploy: 5ceb561612085752505a8062f797ce0baa76a7da

---
+++ create_venv.bat
@@ -0,0 +1,8 @@
+set venv_name=".mvp_demo_venv"
+CALL python -m venv %venv_name%
+cd .mvp_demo_venv\Scripts\
+CALL activate
+cd "../.."
+CALL pip install -r requirements.txt
+@REM CALL jupyter-labextension install @jupyter-widgets/jupyterlab-manager
+CALL python -m ipykernel install --user --name mvp_demo_venv --display-name "Py3 rrap MVP Demo"
