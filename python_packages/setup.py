8d162722b0d1d8a6c1e9aa88818a1360e5906f59
Andrew Freebairn <andrew.freebairn@csiro.au>
2022-07-21 12:05:56 +1000

    WIP: Still trying to fix use of local packages

---
+++ python_packages/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(name='rrap_packages',
+                 version='0.0.1',
+                 description='This package contains python scripts for RRAP-IS functionality',
+                 author='C.S.I.R.O.',
+                 packages=[
+                     'rrap_packages',
+                 ],
+                 install_requires=[
+
+                 ],
+                 url='https://github.com/gbrrestoration/rrap-is-proto',
+                 zip_safe=False)
