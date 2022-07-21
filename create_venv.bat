9be55c8d7e8da3b3c0bf070b542ab05179f0176b
fre171csiro <fre171csiro@users.noreply.github.com>
2022-07-21 01:12:15 +0000

    deploy: 3fb14e9c63725de55a5a3ecd4f1e38c445511734

--- create_venv.bat
+++ create_venv.bat
@@ -3,6 +3,6 @@ CALL python -m venv %venv_name%
 cd .mvp_demo_venv\Scripts\
 CALL activate
 cd "../.."
-CALL pip install -r requirements.txt
-@REM CALL jupyter-labextension install @jupyter-widgets/jupyterlab-manager
+CALL pip install -r requirements_dev.txt
+
 CALL python -m ipykernel install --user --name mvp_demo_venv --display-name "Py3 rrap MVP Demo"
